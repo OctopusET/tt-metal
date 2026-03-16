9f532d917aaf47d72a8ac73eed177259ce0a8fdf
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-03-16 15:52:24 +0000

    SFPI build 353

--- tt_metal/jit_build/build.cpp
+++ tt_metal/jit_build/build.cpp
@@ -147,7 +147,7 @@ void JitBuildEnv::init(
     string common_flags = "-std=c++17 -flto=auto -ffast-math -fno-exceptions ";
 
     if (rtoptions.get_jit_analytics_enabled()) {
-        common_flags += "-fdump-rtl-all -fdump-tree-original ";
+        common_flags += " -fdump-rtl-all -fdump-tree-original ";
     }
 
     if (rtoptions.get_riscv_debug_info_enabled()) {
@@ -476,7 +476,7 @@ void JitBuildState::compile_one(const string& out_dir, const JitBuildSettings* s
     string defines = this->defines_;
 
     if (env_.get_rtoptions().get_build_map_enabled()) {
-        cmd += "-save-temps=obj -fdump-tree-all -fdump-rtl-all ";
+        cmd += "-v -save-temps=obj -fdump-tree-all -fdump-rtl-all ";
     }
 
     if (settings) {
