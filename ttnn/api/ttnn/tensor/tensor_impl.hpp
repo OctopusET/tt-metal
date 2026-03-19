3b159731f7f1a6084b20f25e9c35c3441e2e944f
riverwuTT <riverwu@tenstorrent.com>
2026-03-19 01:17:38 +0000

    migrate pad

--- ttnn/api/ttnn/tensor/tensor_impl.hpp
+++ ttnn/api/ttnn/tensor/tensor_impl.hpp
@@ -131,8 +131,8 @@ HostTensor to_layout(const HostTensor& tensor, Layout target_layout);
 // ======================================================================================
 //                                  .pad() and .unpad()
 // ======================================================================================
-Tensor pad(
-    const Tensor& tensor,
+HostTensor pad(
+    const HostTensor& tensor,
     const tt::tt_metal::Shape& output_padded_shape,
     const tt::tt_metal::Shape& input_tensor_start,
     float pad_value);
